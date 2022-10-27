4f3d0671b2ea40e7c2a6aa8bde6a861111b8e285
GermanKos <kostiakovg@gmail.com>
2022-10-27 18:30:42 +0300

    fixed backdrop bleed ins

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import Backdrop from "components/backdrop/Backdrop";
 import Modal from "components/modal/Modal";
 import Navbar from "components/navbar/Navbar";
 import SignInDemo from "components/sign_in_demo/SignInDemo";
+import { AnimatePresence } from "framer-motion";
 import { useState } from "react";
 import { closeModal, openModal } from "redux/slices/ModalSlice";
 import store from "redux/store";
@@ -13,27 +14,20 @@ function App() {
 
   const closeSignIn = () => store.dispatch(closeModal());
   store.subscribe(() => setSignInOpen(store.getState().rootReducer.modal.open));
-  {
-    /* <Router /> */
-  }
-  {
-    /* <Navbar /> */
-  }
+
   return (
-    <>
-      <h1 style={{ color: "white" }}>aaaa</h1>
-      <input />
-      <button onClick={() => store.dispatch(openModal())}>open</button>
-      {signInOpen && (
-        <Modal
-          modalOpen={signInOpen}
-          handleClose={closeSignIn}
-          component={<SignInDemo />}
-        />
-      )}
-    </>
-    //     <div className="gradient__bg">
-    // </div>
+    <div className="gradient__bg">
+      <Router />
+      <AnimatePresence initial={false}>
+        {signInOpen && (
+          <Modal
+            modalOpen={signInOpen}
+            handleClose={closeSignIn}
+            component={<SignInDemo />}
+          />
+        )}
+      </AnimatePresence>
+    </div>
   );
 }
 
