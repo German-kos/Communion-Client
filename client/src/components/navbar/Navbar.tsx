40916a81e4b2cf2faf0f6cfebcd236405bb19b18
GermanKos <kostiakovg@gmail.com>
2022-10-28 19:51:39 +0300

    small css animations

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -3,11 +3,16 @@ import "components/navbar/navbar.css";
 import store from "redux/store";
 import { closeModal, openModal } from "redux/slices/ModalSlice";
 import { closeSignIn, openSignIn } from "redux/slices/SignInModalSlice";
+import { closeSignUp, openSignUp } from "redux/slices/SignUpModalSlice";
 function Navbar() {
   const [signInOpen, setSignInOpen] = useState<boolean>();
+  const [signUpOpen, setSignUpOpen] = useState<boolean>();
 
   store.subscribe(() =>
     setSignInOpen(store.getState().rootReducer.signInModal.open)
+  );
+  store.subscribe(() =>
+    setSignUpOpen(store.getState().rootReducer.signUpModal.open)
   );
 
   const openSignInModal = () => {
@@ -17,6 +22,12 @@ function Navbar() {
     store.dispatch(closeSignIn());
   };
 
+  const openSignUpModal = () => {
+    store.dispatch(openSignUp());
+  };
+  const closeSignUpModal = () => {
+    store.dispatch(closeSignUp());
+  };
   return (
     <div className="navbar">
       <div className="navbar-links">
@@ -25,13 +36,19 @@ function Navbar() {
           <a>About</a>
         </div>
         <div className="navbar-links_right">
-          <a>Sign Up</a>
           <a
             onClick={() =>
               signInOpen ? closeSignInModal() : openSignInModal()
             }
           >
             Sign In
+          </a>
+          <a
+            onClick={() =>
+              signUpOpen ? closeSignUpModal() : openSignUpModal()
+            }
+          >
+            Sign Up
           </a>
         </div>
       </div>
