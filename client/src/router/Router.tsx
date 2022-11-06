a4d1def62e4dc3027143994237e6488d47db792b
German <KostiakovG@Gmail.com>
2022-11-06 12:36:13 +0200

    import cleanup

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -1,20 +1,10 @@
-import React, { useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from "layout/Layout";
-import UserTest from "components/user test/UserTest";
 import SignInDemo from "components/sign_in_demo/SignInDemo";
 import ProfilePage from "pages/profile/ProfilePage";
-import MyProfilePage from "pages/my_profile/MyProfilePage";
-import { IUser } from "interfaces/UserInterface";
-import store from "redux/store";
-import { current } from "@reduxjs/toolkit";
-function Router() {
-  const myProfileNavigator = () => {
-    const userJson = localStorage.getItem("currentUser");
-    if (userJson) return <MyProfilePage />;
-    return <Navigate to="/" />;
-  };
+import { myProfileNavigator } from "./routerHelpers";
 
+function Router() {
   return (
     <BrowserRouter>
       <Routes>
---
+++ client/src/router/routerHelpers.tsx
@@ -0,0 +1,8 @@
+import MyProfilePage from "pages/my_profile/MyProfilePage";
+import { Navigate } from "react-router-dom";
+
+export const myProfileNavigator = () => {
+  const userJson = localStorage.getItem("currentUser");
+  if (userJson) return <MyProfilePage />;
+  return <Navigate to="/" />;
+};
