f9c303c486dd2ffcce2f213a09085081a0111508
German <KostiakovG@Gmail.com>
2022-11-08 12:19:37 +0200

    some fields for edit profile, added select country

--- client/src/components/my_profile/edit_profile_form/EditProfileForm.tsx
+++ client/src/components/my_profile/edit_profile_form/EditProfileForm.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import "components/my_profile/edit_profile_form/editProfileForm.css";
+import Input from "components/common/input/Input";
+import { inputPcPreset } from "presets/custom_input_presets/input_pc";
+import SelectCountry from "components/common/select_country/SelectCountry";
 
 function EditProfileForm() {
-  return <div>EditProfileForm</div>;
+  const submitForm = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitForm}>
+        <h1>EDIT PROFILE</h1>
+        <SelectCountry />
+        <Input preset={inputPcPreset} placeholder="Bio" name="Bio" key="Bio" />
+      </form>
+    </div>
+  );
 }
 
 export default EditProfileForm;
