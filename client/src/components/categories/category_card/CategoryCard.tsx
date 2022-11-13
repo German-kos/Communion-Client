23ad365ee1164c791c15503d9dc53ba733cb8e01
GermanKos <kostiakovg@gmail.com>
2022-11-13 19:36:27 +0200

    created category card

---
+++ client/src/components/categories/category_card/CategoryCard.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "components/categories/category_card/categoryCard.css";
+
+function CategoryCard() {
+  return <div>CategoryCard</div>;
+}
+
+export default CategoryCard;
